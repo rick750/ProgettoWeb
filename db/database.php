a2666c7541f37d4f79bed8ebfb0241cedfe59f22
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2025-12-30 18:31:48 +0100

    Aggiunti tag

--- db/database.php
+++ db/database.php
@@ -57,7 +57,16 @@ class DatabaseHelper
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    
+    public function getTags(){
+        $query = "SELECT t.nome
+                    FROM TAG t
+                    ";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 
 }
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'bootstrap.php';
 //Base Template
 $templateParams["titolo"] = "Unigames - Home";
 $templateParams["nome"] = "lista-post.php";
+$templateParams["tag"] = $dbh->getTags();
 //Home Template
 $templateParams["post"] = $dbh->getGenericPosts(2);
 $templateParams["recensioni"] = $dbh->getRecensioni(2);
