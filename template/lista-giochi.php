0dd9c78c8f8f8d193ae361b0e824d699f5f19d3f
Marco Battistini <marco.battistini10@studio.unibo.it>
2026-01-02 17:06:43 +0100

    prova immagini

--- template/lista-giochi.php
+++ template/lista-giochi.php
@@ -1,18 +1,21 @@
 <?php foreach($templateParams["libreriaGiochiFunc"] as $gioco): ?>
-<article class="border rounded p-4 text-center bg-light">
-    <header>
-        <h2><?php echo $gioco["nome"]; ?></h2>
-    </header>
-    <section>
-        <p><?php echo $gioco["valutazioneGiornalistica"]; ?></p>
-        <p><?php echo $gioco["listaTag"]; ?></p>
-    </section>
-
-    <div class="d-flex justify-content-end mb-3">
-        <button class="btn btn-primary rounded-pill px-4">
-            Espandi
-        </button>
-    </div>
-
-</article>
+    <article class="card mb-4 shadow-sm border-0 border-start border-4 border-primary">
+        <div>
+            <img class="img-fluid" src="upload/<?php echo $gioco["immagine"]; ?>" alt="immagine di <?php echo $gioco["nome"]; ?>">
+        </div>
+        <div class="card-body">
+            <header class="mb-3">
+                <h2 class="card-title fw-bold mb-1"><?php echo $gioco["nome"]; ?></h2> 
+                <span class="badge bg-primary mb-2">
+                    Valutazione: <?php echo $gioco["valutazioneGiornalistica"]; ?>
+                </span>
+            </header>
+            <section class="card-text">
+                 <p class="fw-semibold mb-2"> Tag: <?php echo $gioco["listaTag"]; ?> </p>
+            </section>
+            <div class="d-flex justify-content-end mt-3">
+                <button class="btn btn-primary rounded-pill px-4"> Espandi </button>
+            </div>
+        </div>
+    </article>
 <?php endforeach; ?>
