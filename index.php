e434059925b199098490181d7c4c298f7855b598
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2026-01-06 16:11:31 +0100

    corretta visualizzazione dei post

--- index.php
+++ index.php
@@ -5,27 +5,9 @@ $templateParams["nome"] = "lista-post.php";
 $templateParams["selezionaFiltro"] = [];
 $templateParams["aside"] = "lista-giochiRandom.php";
 $templateParams["giochiRandomFunc"] = $dbh->getGiochiRandom(3);
-$templateParams["filtri"] = [
-    ["valore" => "generici", "nome" => "Generici"],
-    ["valore" => "recensioni", "nome" => "Recensioni"]
-];
+$templateParams["filtri"] = [];
 
-$templateParams["selezionaFiltro"] = $_GET["filter"] ?? [];
-$filtri = $templateParams["selezionaFiltro"];
-
-$templateParams["post"] = [];
-$templateParams["recensioni"] = [];
-
-$mostraGenerici = empty($filtri) || in_array("generici", $filtri);
-$mostraRecensioni = empty($filtri) || in_array("recensioni", $filtri);
-
-if ($mostraGenerici) {
-    $templateParams["post"] = $dbh->getGenericPosts(2);
-}
-
-if ($mostraRecensioni) {
-    $templateParams["recensioni"] = $dbh->getRecensioni(2);
-}
+$templateParams["post"] = $dbh->getGenericPosts(4);
 
 require 'template/base.php';
 ?>
